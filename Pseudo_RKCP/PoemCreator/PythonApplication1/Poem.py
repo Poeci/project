87d0de003e1fd4280e6b0bc245499fcd4ee4c769
Qovaros <macieknems@gmail.com>
2017-01-15 22:39:43 +0100

    Update Poem.py

--- Pseudo_RKCP/PoemCreator/PythonApplication1/Poem.py
+++ Pseudo_RKCP/PoemCreator/PythonApplication1/Poem.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import Stanza
+#import Stanza
 import sys # uzywane do printowania bez entera, encoding
 from random import randint
 import codecs
@@ -22,18 +22,34 @@ class Poem(object):
             self.resultWords[size-1] = currentWord
             return True
 
-        numberOfNeighbors = len(self.graph[currentWord])
-        for i in range (0, 2*numberOfNeighbors-1):
-            randomNumberOfNeighbor = randint(0, numberOfNeighbors-1)
-            neighbor = self.graph[currentWord][randomNumberOfNeighbor][0]
-            sign = self.graph[currentWord][randomNumberOfNeighbor][1]
-            if self.visitedBy.get(neighbor, "!@#nothin")!=currentWord:  
-                self.visitedBy[neighbor] = currentWord
-                if self.Dfs(neighbor, size+1):
-                    self.resultWords[size-1] = currentWord
-                    self.resultSigns[size-1] = sign
-                    return True
-        return False
+        if(size % 3 != 0):
+            numberOfNeighbors = len(self.graph[currentWord])
+            for i in range (0, 2*numberOfNeighbors-1):
+                randomNumberOfNeighbor = randint(0, numberOfNeighbors-1)
+                neighbor = self.graph[currentWord][randomNumberOfNeighbor][0]
+                sign = self.graph[currentWord][randomNumberOfNeighbor][1]
+                if self.visitedBy.get(neighbor, "!@#nothin")!=currentWord:  
+                    self.visitedBy[neighbor] = currentWord
+                    if self.Dfs(neighbor, size+1):
+                        self.resultWords[size-1] = currentWord
+                        self.resultSigns[size-1] = sign
+                        return True
+            return False            
+
+        if(size % 3 == 0):
+            currentWord = currentWord+'#'+self.visitedBy[currentWord]
+            numberOfNeighbors = len(self.graph[currentWord])
+            for i in range (0, 2*numberOfNeighbors-1):
+                randomNumberOfNeighbor = randint(0, numberOfNeighbors-1)
+                neighbor = self.graph[currentWord][randomNumberOfNeighbor][0]
+                sign = self.graph[currentWord][randomNumberOfNeighbor][1]
+                if self.visitedBy.get(neighbor, "!@#nothin")!=currentWord:  
+                    self.visitedBy[neighbor] = currentWord
+                    if self.Dfs(neighbor, size+1):
+                        self.resultWords[size-1] = currentWord
+                        self.resultSigns[size-1] = sign
+                        return True
+            return False
 
 
     def Generate(self, firstWord):
@@ -46,11 +62,11 @@ class Poem(object):
             return False
         
         for i in range(0, self.howLong-1):
-            wordWithSign = (self.resultWords[i] + self.resultSigns[i]).encode("utf-8")
-            sys.stdout.write(wordWithSign)
+            wordWithSign = self.resultWords[i].encode("utf-8")
+            sys.stdout.write(wordWithSign.split('#')[0])
+            sys.stdout.write(self.resultSigns[i].encode("utf-8"))  
         print ('\n')
         return True
-
 
 
         
